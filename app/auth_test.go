4a800d32fd10a21e2808bdcef773a2bd3a03c66d
Bryan McCarthy <bryanmccarthy2@gmail.com>
2022-12-14 23:10:14 +0000

    create test for auth

---
+++ app/auth_test.go
@@ -0,0 +1,15 @@
+package app
+
+import "testing"
+
+func TestLogin(t *testing.T) {
+	t.Skip("TODO") // TODO: Test Login
+}
+
+func TestLogout(t *testing.T) {
+	t.Skip("TODO") // TODO: Test Logout
+}
+
+func TestRegister(t *testing.T) {
+	t.Skip("TODO") // TODO: Test Register
+}
