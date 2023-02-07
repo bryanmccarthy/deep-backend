2bfa63af1d94fdeb218fcfed5caf98c020e2aeb5
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-02-07 19:18:58 +0000

    add route for updating task current property

---
+++ app/update_task_current.go
@@ -0,0 +1,44 @@
+package app
+
+import (
+	"net/http"
+
+	"github.com/bryanmccarthy/deep-backend/models"
+	"github.com/gin-gonic/gin"
+)
+
+type updateTaskCurrentRequest struct {
+	ID      uint
+	Current bool
+}
+
+func (h handler) updateTaskCurrent(c *gin.Context) {
+	var req updateTaskCurrentRequest
+
+	if err := c.Bind(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	session, err := store.Get(c.Request, "session")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	var task models.Task
+
+	if err := h.DB.Where("id = ? AND user_id = ?", req.ID, session.Values["user_id"]).First(&task).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	task.Current = req.Current
+
+	if err := h.DB.Save(&task).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, &task)
+}
